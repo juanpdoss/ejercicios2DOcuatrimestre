afeda2e7c0b09ef4a7349a1913a04bd0249c3cd5
juanpdoss <juanpdoss@gmail.com>
2021-01-18 14:50:08 -0300

    Ejercicios 2 y 3

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 2";
+
+            int numeroDelUsuario;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero: ");
+         
+
+            } while (!int.TryParse(Console.ReadLine(), out numeroDelUsuario)); //itera hasta que el metodo TryParse retorne true
+
+            //muestro el cuadrado del numero.
+            Console.WriteLine("El cuadrado del numero ingresado es {0}.", Math.Pow(numeroDelUsuario, 2));
+
+            //muestro el cubo del numero ingresado concatenando.
+            Console.WriteLine("El cubo del numero ingresado es " + Math.Pow(numeroDelUsuario,3));
+
+
+
+
+            Console.ReadKey();        
+        
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 3";
+
+            int cantidadIteraciones;
+            int contadorDivisores = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese un numero.");
+                Console.WriteLine("a continuacion, vera los numeros primos existentes entre el 1 y el numero ingresado.");
+
+            } while (!int.TryParse(Console.ReadLine(),out cantidadIteraciones));
+
+
+            for (int i = 1; i <=cantidadIteraciones; i++) //utilizo un for ya que se de antemano cuantas veces tengo que iterar.
+            {
+                for (int j = 1; j <= i; j++) //el for anidado divide a la variable I por cada numero que haya desde el 1 hasta el.
+                {
+                    if (i % j == 0)
+                        contadorDivisores++;
+                }
+
+
+                if (contadorDivisores == 2) //si un numero solo tiene 2 divisores, es primo. 
+                    Console.WriteLine("{0} es primo.", i);
+
+                contadorDivisores = 0; //reinicio el contador.
+
+
+            }
+           
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
